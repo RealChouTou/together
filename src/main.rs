bff3561de1055d3ae7d9513d03f9a65c10affc10
RealChouTou <1984or2077@gmail.com>
2024-12-19 13:28:55 +0900

    base

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use chrono::prelude::*;
+use chrono::Utc;
 use iced::widget::{button, horizontal_space, text, text_editor, text_input, Column, Container};
-use iced::{Background, Center, Color, Element, Length, Task};
+use iced::{time, Background, Center, Color, Element, Length, Subscription, Task};
 use rfd::{AsyncFileDialog, FileDialog};
 use std::process::Command;
-
 #[derive(Default)]
 struct App {
     watch_time: String,
     start_time: String,
     full_screen: bool,
     file_path: String,
+    schedule_running: bool,
+    info: String,
 }
 
 #[derive(Debug, Clone)]
@@ -19,6 +22,7 @@ pub enum Message {
     FileSelected(Option<String>),
     InputChanged(String),
     WatchTimeChanged(String),
+    Tick,
 }
 impl App {
     pub fn view(&self) -> Element<Message> {
@@ -58,27 +62,19 @@ impl App {
             .center_y(Length::Fill)
             .into()
     }
-
+    fn subscription(&self) -> Subscription<Message> {
+        // 每秒触发一次 TimerTick 消息 (如果需要定时重复输出，可以保留此行)
+        if self.schedule_running {
+            time::every(time::Duration::from_millis(1)).map(|_| Message::Tick)
+        } else {
+            Subscription::none()
+        }
+    }
     pub fn update(&mut self, message: Message) -> Task<Message> {
         match message {
             Message::Start => {
-                println!("{} {}", "Start", self.file_path);
-                let command = format!(
-                    "{}{} {}",
-                    "vlc --fullscreen --start-time=",
-                    self.start_time,
-                    self.file_path.replace("\\", "\\\\")
-                );
-                println!("{}", command);
-                let output = Command::new("cmd")
-                    .args(&["/C", command.as_str()]) // /C 参数用于运行单个命令并退出
-                    .output()
-                    .expect("Failed to execute command");
-
-                // 将输出转换为字符串并打印
-                println!("Status: {}", output.status);
-                println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
-                println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+                self.schedule_running = true;
+                println!("please wait");
                 Task::none()
             }
             Message::OpenFile => {
@@ -107,10 +103,46 @@ impl App {
                 self.watch_time = input;
                 Task::none()
             }
+            Message::Tick => {
+                let now = Local::now();
+                let hour = now.hour();
+                let minute = now.minute();
+                println!("hour: {} minute:{}", hour, minute);
+                // let second = now.second();
+                // let nanosecond = now.nanosecond(); // 纳秒
+                // let millisecond = nanosecond / 1_000_000; // 毫秒
+                let split: Vec<&str> = self.watch_time.split(":").collect::<Vec<_>>();
+                let plan_hour = split[0].parse::<u32>().unwrap();
+                let plan_minute = split[1].parse::<u32>().unwrap();
+                let go = plan_hour == hour && plan_minute == minute;
+                if go {
+                    let command = format!(
+                        "{}{} {}",
+                        "vlc --fullscreen --start-time=",
+                        self.start_time,
+                        self.file_path.replace("\\", "\\\\")
+                    );
+                    println!("{}", command);
+                    let output = Command::new("cmd")
+                        .args(&["/C", command.as_str()]) // /C 参数用于运行单个命令并退出
+                        .output()
+                        .expect("Failed to execute command");
+
+                    // 将输出转换为字符串并打印
+                    println!("Status: {}", output.status);
+                    println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+                    println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+                    self.schedule_running = false;
+                }
+
+                Task::none()
+            }
             _ => Task::none(),
         }
     }
 }
 fn main() -> iced::Result {
-    iced::run("Together", App::update, App::view)
+    iced::application("Together", App::update, App::view)
+        .subscription(App::subscription)
+        .run()
 }
